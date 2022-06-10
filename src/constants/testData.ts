54e75e589b0dadd9a47f2e40dbd79d6ee7ac869b
Toan Tran <toantdfu@gmail.com>
2022-06-11 07:59:34 +0900

    Correct test data

--- src/constants/testData.ts
+++ src/constants/testData.ts
@@ -1,77 +1,223 @@
 export const products = [
   {
-    id: '3bb8464e-e85f-11ec-8fea-0242ac120002',
-    title: 'Yellow Pail',
-    description: 'On-demand sand castle construction expertise.',
-    url: '#',
-    votes: 2,
-    submitterAvatarUrl: 'images/avatars/daniel.jpg',
-    productImageUrl: 'images/products/image-aqua.png',
+    id: 'b569a741-5350-419d-ac27-f4d1a247f9d7',
+    title: 'Licensed Metal Soap',
+    description:
+      'Andy shoes are designed to keeping in mind durability as well as trends, the most stylish range of shoes & sandals',
+    url: 'https://grizzled-leader.org',
+    votes: 76,
+    submitterAvatarUrl:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/735.jpg',
+    productImageUrl: 'https://loremflickr.com/200/200/product?25809',
   },
   {
-    id: '47f524f4-e85f-11ec-8fea-0242ac120002',
-    title: 'Supermajority: The Fantasy Congress League',
-    description: 'Earn points when your favorite politicians pass legislation.',
-    url: '#',
-    votes: 5,
-    submitterAvatarUrl: 'images/avatars/kristy.png',
-    productImageUrl: 'images/products/image-rose.png',
+    id: '3c44dd77-3ab1-4439-8f97-b86ab5406be2',
+    title: 'Rustic Frozen Bacon',
+    description:
+      "Boston's most advanced compression wear technology increases muscle oxygenation, stabilizes active muscles",
+    url: 'http://parched-sticker.biz',
+    votes: 14,
+    submitterAvatarUrl:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1141.jpg',
+    productImageUrl: 'https://loremflickr.com/200/200/product?94065',
   },
   {
-    id: '54efe482-e85f-11ec-8fea-0242ac120002',
-    title: 'Tinfoild: Tailored tinfoil hats',
-    description: 'We already have your measurements and shipping address.',
-    url: '#',
-    votes: 1,
-    submitterAvatarUrl: 'images/avatars/veronika.jpg',
-    productImageUrl: 'images/products/image-steel.png',
+    id: '145e8bad-c078-4d68-ad30-581d12f6f355',
+    title: 'Bespoke Granite Bacon',
+    description:
+      'Ergonomic executive chair upholstered in bonded black leather and PVC padded seat and back for all-day comfort and support',
+    url: 'http://valuable-nightlife.info',
+    votes: 61,
+    submitterAvatarUrl:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/977.jpg',
+    productImageUrl: 'https://loremflickr.com/200/200/product?93128',
   },
   {
-    id: '64f172e2-e85f-11ec-8fea-0242ac120002',
-    title: 'Haught or Naught',
-    description: 'High-minded or absent-minded? You decide.',
-    url: '#',
-    votes: 5,
-    submitterAvatarUrl: 'images/avatars/molly.png',
-    productImageUrl: 'images/products/image-yellow.png',
+    id: '8ac75313-89a0-44b4-a434-d1dd9aa6783f',
+    title: 'Practical Wooden Pants',
+    description:
+      'New range of formal shirts are designed keeping you in mind. With fits and styling that will make you stand apart',
+    url: 'http://leafy-nick.biz',
+    votes: 88,
+    submitterAvatarUrl:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/227.jpg',
+    productImageUrl: 'https://loremflickr.com/200/200/product?12961',
+  },
+  {
+    id: 'ab706e12-c496-4ba9-bc09-7f3c082c5493',
+    title: 'Tasty Bronze Shoes',
+    description:
+      'New ABC 13 9370, 13.3, 5th Gen CoreA5-8250U, 8GB RAM, 256GB SSD, power UHD Graphics, OS 10 Home, OS Office A & J 2016',
+    url: 'https://crafty-particle.org',
+    votes: 12,
+    submitterAvatarUrl:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/954.jpg',
+    productImageUrl: 'https://loremflickr.com/200/200/product?29740',
+  },
+  {
+    id: '48ea9aaf-3346-4fb3-937d-a61fb49b296e',
+    title: 'Fantastic Plastic Fish',
+    description:
+      "Boston's most advanced compression wear technology increases muscle oxygenation, stabilizes active muscles",
+    url: 'https://trusting-disruption.name',
+    votes: 59,
+    submitterAvatarUrl:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1080.jpg',
+    productImageUrl: 'https://loremflickr.com/200/200/product?94449',
+  },
+  {
+    id: 'abe97b68-4f15-4d18-847e-45df9e12b247',
+    title: 'Electronic Soft Table',
+    description:
+      'New range of formal shirts are designed keeping you in mind. With fits and styling that will make you stand apart',
+    url: 'https://brief-fibrosis.info',
+    votes: 15,
+    submitterAvatarUrl:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/94.jpg',
+    productImageUrl: 'https://loremflickr.com/200/200/product?5742',
+  },
+  {
+    id: '73360778-7bbe-490f-827a-10f7ed3d06a7',
+    title: 'Awesome Bronze Bacon',
+    description: 'The Football Is Good For Training And Recreational Purposes',
+    url: 'https://jaunty-lever.biz',
+    votes: 97,
+    submitterAvatarUrl:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/410.jpg',
+    productImageUrl: 'https://loremflickr.com/200/200/product?38903',
+  },
+  {
+    id: '4d538942-1dcb-494e-9219-7f3be959e501',
+    title: 'Tasty Metal Salad',
+    description:
+      'The automobile layout consists of a front-engine design, with transaxle-type transmissions mounted at the rear of the engine and four wheel drive',
+    url: 'http://artistic-perspective.name',
+    votes: 97,
+    submitterAvatarUrl:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/975.jpg',
+    productImageUrl: 'https://loremflickr.com/200/200/product?61015',
+  },
+  {
+    id: '41000b21-a80c-41d9-8194-e1cd550a034e',
+    title: 'Ergonomic Rubber Computer',
+    description:
+      'Carbonite web goalkeeper gloves are ergonomically designed to give easy fit',
+    url: 'http://happy-trailer.info',
+    votes: 17,
+    submitterAvatarUrl:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/321.jpg',
+    productImageUrl: 'https://loremflickr.com/200/200/product?20074',
   },
 ];
 
 export const sortedProducts = [
   {
-    id: '47f524f4-e85f-11ec-8fea-0242ac120002',
-    title: 'Supermajority: The Fantasy Congress League',
-    description: 'Earn points when your favorite politicians pass legislation.',
-    url: '#',
-    votes: 5,
-    submitterAvatarUrl: 'images/avatars/kristy.png',
-    productImageUrl: 'images/products/image-rose.png',
+    id: '73360778-7bbe-490f-827a-10f7ed3d06a7',
+    title: 'Awesome Bronze Bacon',
+    description: 'The Football Is Good For Training And Recreational Purposes',
+    url: 'https://jaunty-lever.biz',
+    votes: 97,
+    submitterAvatarUrl:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/410.jpg',
+    productImageUrl: 'https://loremflickr.com/200/200/product?38903',
   },
   {
-    id: '64f172e2-e85f-11ec-8fea-0242ac120002',
-    title: 'Haught or Naught',
-    description: 'High-minded or absent-minded? You decide.',
-    url: '#',
-    votes: 5,
-    submitterAvatarUrl: 'images/avatars/molly.png',
-    productImageUrl: 'images/products/image-yellow.png',
+    id: '4d538942-1dcb-494e-9219-7f3be959e501',
+    title: 'Tasty Metal Salad',
+    description:
+      'The automobile layout consists of a front-engine design, with transaxle-type transmissions mounted at the rear of the engine and four wheel drive',
+    url: 'http://artistic-perspective.name',
+    votes: 97,
+    submitterAvatarUrl:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/975.jpg',
+    productImageUrl: 'https://loremflickr.com/200/200/product?61015',
   },
   {
-    id: '3bb8464e-e85f-11ec-8fea-0242ac120002',
-    title: 'Yellow Pail',
-    description: 'On-demand sand castle construction expertise.',
-    url: '#',
-    votes: 2,
-    submitterAvatarUrl: 'images/avatars/daniel.jpg',
-    productImageUrl: 'images/products/image-aqua.png',
+    id: '8ac75313-89a0-44b4-a434-d1dd9aa6783f',
+    title: 'Practical Wooden Pants',
+    description:
+      'New range of formal shirts are designed keeping you in mind. With fits and styling that will make you stand apart',
+    url: 'http://leafy-nick.biz',
+    votes: 88,
+    submitterAvatarUrl:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/227.jpg',
+    productImageUrl: 'https://loremflickr.com/200/200/product?12961',
   },
   {
-    id: '54efe482-e85f-11ec-8fea-0242ac120002',
-    title: 'Tinfoild: Tailored tinfoil hats',
-    description: 'We already have your measurements and shipping address.',
-    url: '#',
-    votes: 1,
-    submitterAvatarUrl: 'images/avatars/veronika.jpg',
-    productImageUrl: 'images/products/image-steel.png',
+    id: 'b569a741-5350-419d-ac27-f4d1a247f9d7',
+    title: 'Licensed Metal Soap',
+    description:
+      'Andy shoes are designed to keeping in mind durability as well as trends, the most stylish range of shoes & sandals',
+    url: 'https://grizzled-leader.org',
+    votes: 76,
+    submitterAvatarUrl:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/735.jpg',
+    productImageUrl: 'https://loremflickr.com/200/200/product?25809',
+  },
+  {
+    id: '145e8bad-c078-4d68-ad30-581d12f6f355',
+    title: 'Bespoke Granite Bacon',
+    description:
+      'Ergonomic executive chair upholstered in bonded black leather and PVC padded seat and back for all-day comfort and support',
+    url: 'http://valuable-nightlife.info',
+    votes: 61,
+    submitterAvatarUrl:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/977.jpg',
+    productImageUrl: 'https://loremflickr.com/200/200/product?93128',
+  },
+  {
+    id: '48ea9aaf-3346-4fb3-937d-a61fb49b296e',
+    title: 'Fantastic Plastic Fish',
+    description:
+      "Boston's most advanced compression wear technology increases muscle oxygenation, stabilizes active muscles",
+    url: 'https://trusting-disruption.name',
+    votes: 59,
+    submitterAvatarUrl:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1080.jpg',
+    productImageUrl: 'https://loremflickr.com/200/200/product?94449',
+  },
+  {
+    id: '41000b21-a80c-41d9-8194-e1cd550a034e',
+    title: 'Ergonomic Rubber Computer',
+    description:
+      'Carbonite web goalkeeper gloves are ergonomically designed to give easy fit',
+    url: 'http://happy-trailer.info',
+    votes: 17,
+    submitterAvatarUrl:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/321.jpg',
+    productImageUrl: 'https://loremflickr.com/200/200/product?20074',
+  },
+  {
+    id: 'abe97b68-4f15-4d18-847e-45df9e12b247',
+    title: 'Electronic Soft Table',
+    description:
+      'New range of formal shirts are designed keeping you in mind. With fits and styling that will make you stand apart',
+    url: 'https://brief-fibrosis.info',
+    votes: 15,
+    submitterAvatarUrl:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/94.jpg',
+    productImageUrl: 'https://loremflickr.com/200/200/product?5742',
+  },
+  {
+    id: '3c44dd77-3ab1-4439-8f97-b86ab5406be2',
+    title: 'Rustic Frozen Bacon',
+    description:
+      "Boston's most advanced compression wear technology increases muscle oxygenation, stabilizes active muscles",
+    url: 'http://parched-sticker.biz',
+    votes: 14,
+    submitterAvatarUrl:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1141.jpg',
+    productImageUrl: 'https://loremflickr.com/200/200/product?94065',
+  },
+  {
+    id: 'ab706e12-c496-4ba9-bc09-7f3c082c5493',
+    title: 'Tasty Bronze Shoes',
+    description:
+      'New ABC 13 9370, 13.3, 5th Gen CoreA5-8250U, 8GB RAM, 256GB SSD, power UHD Graphics, OS 10 Home, OS Office A & J 2016',
+    url: 'https://crafty-particle.org',
+    votes: 12,
+    submitterAvatarUrl:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/954.jpg',
+    productImageUrl: 'https://loremflickr.com/200/200/product?29740',
   },
 ];
